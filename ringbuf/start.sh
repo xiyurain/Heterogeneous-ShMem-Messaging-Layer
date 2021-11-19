bb0e866f2699e079c9bb8cd60c9cbc6abb156ade
Xiangyu Ren <renxiangyu@sensetime.com>
2021-11-19 18:25:34 +0800

    add MSI-X interrupt

--- ringbuf/start.sh
+++ ringbuf/start.sh
@@ -1,1 +1,12 @@
-qemu-system-x86_64 -m 1024M -drive format=raw,file=rootfs.img -kernel ../images/bzImage -append "root=/dev/sda init=/bin/ash" -object memory-backend-file,size=4M,share,mem-path=/dev/shm/ivshmem,id=hostmem -device ivshmem-plain,memdev=hostmem
+ivshmem-server -l 4M -M fg-doorbell -n 8 -F -v
+
+ivshmem-client
+dump
+
+qemu-system-x86_64 -m 1024M \
+    -drive format=raw,file=rootfs.img \
+    -kernel ../images/bzImage \
+    -append "root=/dev/sda init=/bin/ash" \
+    -chardev socket,path=/tmp/ivshmem_socket,id=fg-doorbell \
+    -device ivshmem-doorbell,chardev=fg-doorbell,vectors=8
+    
