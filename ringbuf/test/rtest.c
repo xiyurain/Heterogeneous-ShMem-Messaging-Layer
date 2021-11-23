b816593d2bbf4ff4a5bb5d37d21952698dbdfd4a
Xiangyu Ren <renxiangyu@sensetime.com>
2021-11-23 22:18:02 +0800

    read test

--- ringbuf/test/rtest.c
+++ ringbuf/test/rtest.c
@@ -5,21 +5,25 @@
  
 int __init rtest_init(void)
 {
-    struct file *fp;
-    printk("hello enter/n");
+	struct file *fp;
+	mm_segment_t old_fs;
+	char msg_buffer[64];
 
-    fp = filp_open("/dev/ringbuf", O_RDWR, 0644);
-    if (IS_ERR(fp)){
-        printk("create file error/n");
-        return -1;
-    }
+    	printk("hello enter/n");
 
-    filp_close(fp,NULL);
-    return 0;
+    	fp = filp_open("/dev/ringbuf", O_RDWR, 0644);
+    	old_fs = get_fs();
+	set_fs(KERNEL_DS);
+
+	vfs_read(fp, msg_buffer, 63, 0);
+
+	set_fs(old_fs); 
+	filp_close(fp,NULL);
+	return 0;
 }
 void __exit rtest_exit(void)
 {
-    printk("rtest exit/n");
+    	printk("rtest exit/n");
 }
  
 module_init(rtest_init);
